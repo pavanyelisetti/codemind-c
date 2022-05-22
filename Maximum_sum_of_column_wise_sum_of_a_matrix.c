9770f4182ad5340e22c8bb81c940a589e34ff4d4
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-05-22 16:58:49 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d%d",&r,&c);     int a[100][100],i,j,sum,max=0;     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(j=0;j<c;j++)     {         sum=0;         for(i=0;i<r;i++)         {             sum=sum+a[i][j];         }         if(sum>max)         {             max=sum;         }     }     printf("%d",max); }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int a[100][100],i,j,sum,max=0;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(j=0;j<c;j++)
+    {
+        sum=0;
+        for(i=0;i<r;i++)
+        {
+            sum=sum+a[i][j];
+        }
+        if(sum>max)
+        {
+            max=sum;
+        }
+    }
+    printf("%d",max);
+}
