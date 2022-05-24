86c8a33196981f04ec8cd3e316fce19cc4f1b3b5
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-05-24 21:59:46 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[100],i,rev=0,count=0,temp,r;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         rev=0;         temp=a[i];         while(temp>0)         {             r=temp%10;             rev=rev*10+r;             temp=temp/10;         }         if(a[i]==rev)         {             count++;         }     }     printf("%d",count); }

---
+++ Count_palindromes.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[100],i,rev=0,count=0,temp,r;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        rev=0;
+        temp=a[i];
+        while(temp>0)
+        {
+            r=temp%10;
+            rev=rev*10+r;
+            temp=temp/10;
+        }
+        if(a[i]==rev)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
