c20c0e0975790119da5189017bb65ce68c9c689b
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-06-20 19:26:12 +0530

    #include<stdio.h> int main() {     int n,s=0,i;     scanf("%d",&n);     int a[100];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]<a[i+1])         {
                s=s+1;
            }
        }
        if(s==n-1)
        {
            printf("yes");
        }
        else
        {
            printf("no");
        }
    }

---
+++ Ascending_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0,i;
+    scanf("%d",&n);
+    int a[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<a[i+1])
+        {
+            
+            s=s+1;
+        }
+    }
+    if(s==n-1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
