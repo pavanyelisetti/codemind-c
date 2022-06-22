d747812f1ae31cfae9cf7c94850300419731e4a3
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-06-22 19:54:19 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,temp,count,count1,i,j,k,m,s,x,y,r,f;     scanf("%d",&n);     for(i=n;i>=0;i--)     {             count=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 {                     count++;                 }             }             if(count==2)             {                 x=i;                 s=1;                 break;             }             else             {                 continue;             }     }     k=n;     while(k>0)     {         count1=0;         for(i=1;i<=k;i++)         {             if(k%i==0)             {                 count1++;             }         }         if(count1==2)         {             y=k;             m=1;             break;         }         else         {             k++;         }     }     f=abs(n-x);     r=abs(n-y);     if(f<r)     {         printf("%d ",f);     }     else     {         printf("%d ",r);     }
    }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,temp,count,count1,i,j,k,m,s,x,y,r,f;
+    scanf("%d",&n);
+    for(i=n;i>=0;i--)
+    {
+            count=0;
+            for(j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    count++;
+                }
+            }
+            if(count==2)
+            {
+                x=i;
+                s=1;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+    }
+    k=n;
+    while(k>0)
+    {
+        count1=0;
+        for(i=1;i<=k;i++)
+        {
+            if(k%i==0)
+            {
+                count1++;
+            }
+        }
+        if(count1==2)
+        {
+            y=k;
+            m=1;
+            break;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    f=abs(n-x);
+    r=abs(n-y);
+    if(f<r)
+    {
+        printf("%d
+",f);
+    }
+    else
+    {
+        printf("%d
+",r);
+    }
+
+}
