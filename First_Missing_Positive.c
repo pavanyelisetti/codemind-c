ca77504f82d5615892ff29051fb6d494dc83af00
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-09-28 08:25:56 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[100],i,j,max=0,c=0,f=0,k;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(max<=a[i])         {             max=a[i];         }     }     for(k=1;k<=max;k++)     {         c=0;         for(j=0;j<n;j++)         {             if(k!=a[j])             {                 c+=1;             }         }         if(c==n)         {             printf("%d",k);             f=1;             break;         }     }     if(f==0)     {         printf("%d",(max+1));     } }

---
+++ First_Missing_Positive.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[100],i,j,max=0,c=0,f=0,k;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max<=a[i])
+        {
+            max=a[i];
+        }
+    }
+    for(k=1;k<=max;k++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(k!=a[j])
+            {
+                c+=1;
+            }
+        }
+        if(c==n)
+        {
+            printf("%d",k);
+            f=1;
+            break;
+        }
+    }
+    if(f==0)
+    {
+        printf("%d",(max+1));
+    }
+}
