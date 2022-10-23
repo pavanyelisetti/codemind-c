46eb33d8aba4fef010b6a2a570f9ec9e5a1ac274
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-10-23 16:11:11 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(k=1;k<=n;k++)         {             if(i==1 || i==n || k==1|| k==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } } /*n=int(input()) for i in range(1,n+1):     for j in range(n,i,-1):         print(" ",end="")     for l in range(0,n):         if(i==1 or i==n or l==0 or l==n-1):             print("*",end="")         else:             print(" ",end="")     print()*/

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,k;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n-2;j++)
+        for(j=1;j<=n-i;j++)
         {
-            printf("%d",j);
+            printf(" ");
         }
-        for(j=n-3;j>=1;j--)
+        for(k=1;k<=n;k++)
         {
-            printf("%d",j);
+            if(i==1 || i==n || k==1|| k==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
     }
 }
+/*n=int(input())
+for i in range(1,n+1):
+    for j in range(n,i,-1):
+        print(" ",end="")
+    for l in range(0,n):
+        if(i==1 or i==n or l==0 or l==n-1):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()*/
